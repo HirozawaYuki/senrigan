d9cc6cbae6901d68795de40dbb0571eb80488b18
ebifly0001 <jiaweidouc@gmail.com>
2020-07-22 20:37:56 +0900

    データベースに接続するアカウント情報を変更しました

--- dbconnect.php
+++ dbconnect.php
@@ -3,8 +3,8 @@
     function exeSQL($sql){
         try {
             $dsn = 'mysql:dbname=senrigan;host=localhost';
-            $username = 'root';
-            $password = '17854tak03101112';
+            $username = 'super_user';
+            $password = 'antipg0246';
             $dbh = new PDO($dsn, $username, $password); //データベースに接続
             $dbh->query('SET NAMES utf8'); //文字コードのための設定
             $stmt = $dbh->prepare($sql); //sql文を実行する準備
