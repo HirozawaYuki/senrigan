30a7c72a21f8e11dcbeb2f71dde56eae0c0a5479
KaitoAkagi <jiaweidouc@gmail.com>
2020-08-08 17:31:08 +0900

    メイン画面の表示くずれを解決しました

--- home/js/show-users.js
+++ home/js/show-users.js
@@ -23,41 +23,67 @@ function showUsers(users){
       absent_num += 1;
     }
   }
+
+  //現在時刻の取得
+  var date=new Date();
+  var hour=date.getHours();
+
+  var color_list=["red","green","blue","red","blue","green"];
   
   var member_concat = attendance_list.concat(absent_list); //在室者がリストの先頭に来るようにリストを結合
   
   for (var num = 0; num < member_concat.length; num++) {
     // createElement:タグの生成　<button></button>
-    var div = document.createElement("div")
+    var div = document.createElement("div");
+    var p0 = document.createElement("p");
     var p1 = document.createElement("p");
     var p2 = document.createElement("p");
     var b1 = document.createElement("button");
+
+    var p_time=document.createElement("p");
+
     // innerHTMLを用いることで要素の中身を変更することができる
     p1.innerHTML = member_concat[num];
     // setAttribute:タグの属性の設定
     p1.setAttribute("id", num);
     // var check_attendance = decision_class();
-    
+    div.setAttribute("class","flex_container");
+
     /*在室者は前半に置いてあるはずなので、総在室者の数に達するまでは在室として定義*/
     if (attend_num > num) {
       p1.setAttribute("class", "square_attendance");
-      p2.innerHTML = "在室";
+      // p2.innerHTML = "在室";
       p2.setAttribute("id", "attendance");
     } else {
       p1.setAttribute("class", "square_absent");
-      p2.innerHTML = "不在";
+      // p2.innerHTML = "不在";
       p2.setAttribute("id", "absent");
     }
     b1.setAttribute("id","button");
-    b1.innerHTML="";
+    //b1.innerHTML="";
+
+    //時間の表示
+    p_time.innerHTML=hour.toString()+"時間前";
+    p_time.setAttribute("class","log_time");
+
+    p0.setAttribute("class","user_color");
+    p0.setAttribute("style","background-color:"+color_list[num]+"");
+
     //appendChild:HTMLに設定済みのタグを挿入
-    document.getElementById("main").appendChild(p1);
-    document.getElementById("main").appendChild(p2);
-    document.getElementById("main").appendChild(b1);
+    document.getElementById("main").appendChild(div);
+    div.appendChild(p0);
+    div.appendChild(p1);
+    div.appendChild(p2);
+    div.appendChild(b1);
+    div.appendChild(p_time);
+    // document.getElementById("main").appendChild(p1);
+    // document.getElementById("main").appendChild(p2);
+    // document.getElementById("main").appendChild(b1);
     var left_pos = 5;
     var top_pos = 100;
     document.getElementById(num).style.left = "" + left_pos + "%";
     document.getElementById(num).style.top = "" + top_pos + "px";
   }
+  //document.getElementById("main").appendChild(p_time);
   
 }
